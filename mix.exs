e1532a05b359333b4cc930eac08e6edda9609629
teerawat1992 <teerawat.lamanchart@gmail.com>
2018-10-06 21:29:10 +0700

    Configure travis ci

--- mix.exs
+++ mix.exs
@@ -5,20 +5,18 @@ defmodule Zxcvbn.MixProject do
     [
       app: :zxcvbn,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:credo, "~> 0.10.2", only: [:dev, :test], runtime: false}
