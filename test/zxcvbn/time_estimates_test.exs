7331ddfcf064cfd1268a54b05c5f77511b07c48b
Kris Lamote <kris.lamote@it-kitchen.be>
2018-10-09 23:07:36 +0200

    apply credo refactoring suggestions

--- test/zxcvbn/time_estimates_test.exs
+++ test/zxcvbn/time_estimates_test.exs
@@ -9,19 +9,37 @@ defmodule Zxcvbn.TimeEstimatesTest do
 
   test "anything between 1 second and several years" do
     assert 35 |> TimeEstimates.display_time() == "35 seconds"
-    assert (2 * 60 + 35) |> TimeEstimates.display_time() == "2 minutes"
-    assert (4 * 60 * 60 + 35) |> TimeEstimates.display_time() == "4 hours"
-    assert (2 * 24 * 60 * 60 + 35) |> TimeEstimates.display_time() == "2 days"
-    assert (4 * 31 * 24 * 60 * 60 + 35) |> TimeEstimates.display_time() == "4 months"
-    assert (5 * 365 * 24 * 60 * 60 + 35) |> TimeEstimates.display_time() == "5 years"
+
+    two_mins = 2 * 60 + 35
+    assert two_mins |> TimeEstimates.display_time() == "2 minutes"
+
+    four_hours = 4 * 60 * 60 + 35
+    assert four_hours |> TimeEstimates.display_time() == "4 hours"
+
+    two_days = 2 * 24 * 60 * 60 + 35
+    assert two_days |> TimeEstimates.display_time() == "2 days"
+
+    four_months = 4 * 31 * 24 * 60 * 60 + 35
+    assert four_months |> TimeEstimates.display_time() == "4 months"
+
+    five_years = 5 * 365 * 24 * 60 * 60 + 35
+    assert five_years |> TimeEstimates.display_time() == "5 years"
   end
 
   test "anything whenever base is 1" do
     assert 1 |> TimeEstimates.display_time() == "1 second"
     assert 60 |> TimeEstimates.display_time() == "1 minute"
-    assert (60 * 60 + 35) |> TimeEstimates.display_time() == "1 hour"
-    assert (24 * 60 * 60) |> TimeEstimates.display_time() == "1 day"
-    assert (31 * 24 * 60 * 60) |> TimeEstimates.display_time() == "1 month"
-    assert (365 * 24 * 60 * 60) |> TimeEstimates.display_time() == "1 year"
+
+    hour = 60 * 60
+    assert hour |> TimeEstimates.display_time() == "1 hour"
+
+    day = 24 * 60 * 60
+    assert day |> TimeEstimates.display_time() == "1 day"
+
+    month = 31 * 24 * 60 * 60
+    assert month |> TimeEstimates.display_time() == "1 month"
+
+    year = 365 * 24 * 60 * 60
+    assert year |> TimeEstimates.display_time() == "1 year"
   end
 end
