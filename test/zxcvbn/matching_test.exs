e464e9fbc2fc89ceeefd2e64746afafe184a27c3
Kris Lamote <kris.lamote@it-kitchen.be>
2018-10-16 22:05:06 +0200

    dictionary match

--- test/zxcvbn/matching_test.exs
+++ test/zxcvbn/matching_test.exs
@@ -1,3 +1,206 @@
 defmodule Zxcvbn.MatchingTest do
   use ExUnit.Case
+
+  alias Zxcvbn.Matching
+
+  describe "dictionary_match/2" do
+    # @tag :pending
+    test "empty passwords has no matches" do
+      dicts = [%{name: "d1", words: %{"motherboard" => 1}}]
+
+      matches = Matching.dictionary_match("", dicts)
+      assert Enum.empty?(matches)
+    end
+
+    # @tag :pending
+    test "empty dictionary has no matches" do
+      matches = Matching.dictionary_match("password", [])
+      assert Enum.empty?(matches)
+    end
+
+    # @tag :pending
+    test "matches words that contain other words" do
+      matches = Matching.dictionary_match("motherboard", dictionaries())
+      assert Enum.count(matches) == 3
+
+      {match, matches} = List.pop_at(matches, 0)
+
+      expected = %{
+        dict_match()
+        | i: 0,
+          j: 5,
+          rank: 2,
+          token: "mother",
+          matched_word: "mother",
+          dictionary_name: "d1"
+      }
+
+      assert match == expected
+
+      {match, matches} = List.pop_at(matches, 0)
+
+      expected = %{
+        expected
+        | i: 0,
+          j: 10,
+          rank: 1,
+          token: "motherboard",
+          matched_word: "motherboard"
+      }
+
+      assert match == expected
+
+      {match, matches} = List.pop_at(matches, 0)
+      expected = %{expected | i: 6, j: 10, rank: 3, token: "board", matched_word: "board"}
+      assert match == expected
+    end
+
+    # @tag :pending
+    test "matches multiple words when they overlap" do
+      matches = Matching.dictionary_match("abcdef", dictionaries())
+      assert Enum.count(matches) == 2
+
+      {match, matches} = List.pop_at(matches, 0)
+
+      expected = %{
+        dict_match()
+        | i: 0,
+          j: 3,
+          rank: 4,
+          token: "abcd",
+          matched_word: "abcd",
+          dictionary_name: "d1"
+      }
+
+      assert match == expected
+
+      {match, matches} = List.pop_at(matches, 0)
+      expected = %{expected | i: 2, j: 5, rank: 5, token: "cdef", matched_word: "cdef"}
+      assert match == expected
+    end
+
+    # @tag :pending
+    test "ignores uppercasing" do
+      matches = Matching.dictionary_match("BoaRdZ", dictionaries())
+      assert Enum.count(matches) == 2
+
+      {match, matches} = List.pop_at(matches, 0)
+
+      expected = %{
+        dict_match()
+        | i: 0,
+          j: 4,
+          rank: 3,
+          token: "BoaRd",
+          matched_word: "board",
+          dictionary_name: "d1"
+      }
+
+      assert match == expected
+
+      {match, matches} = List.pop_at(matches, 0)
+
+      expected = %{
+        expected
+        | i: 5,
+          j: 5,
+          rank: 1,
+          token: "Z",
+          matched_word: "z",
+          dictionary_name: "d2"
+      }
+
+      assert match == expected
+    end
+
+    # @tag :pending
+    test "identifies words surrounded by non-words" do
+      word = "asdf1234&*"
+
+      matches = Matching.dictionary_match("q" <> word, dictionaries())
+      {match, matches} = List.pop_at(matches, 0)
+
+      expected = %{
+        dict_match()
+        | i: 1,
+          j: 10,
+          rank: 5,
+          token: "asdf1234&*",
+          matched_word: "asdf1234&*",
+          dictionary_name: "d2"
+      }
+
+      assert match == expected
+
+      matches = Matching.dictionary_match("%" <> word, dictionaries())
+      {match, matches} = List.pop_at(matches, 0)
+      assert match == expected
+
+      matches = Matching.dictionary_match("q" <> word <> "%%", dictionaries())
+      {match, matches} = List.pop_at(matches, 0)
+      assert match == expected
+
+      matches = Matching.dictionary_match("%" <> word <> "pp", dictionaries())
+      {match, matches} = List.pop_at(matches, 0)
+      assert match == expected
+
+      matches = Matching.dictionary_match(word <> "%%", dictionaries())
+      {match, matches} = List.pop_at(matches, 0)
+
+      expected = %{
+        dict_match()
+        | i: 0,
+          j: 9,
+          rank: 5,
+          token: "asdf1234&*",
+          matched_word: "asdf1234&*",
+          dictionary_name: "d2"
+      }
+
+      assert match == expected
+
+      matches = Matching.dictionary_match(word <> "qq", dictionaries())
+      {match, matches} = List.pop_at(matches, 0)
+      assert match == expected
+    end
+
+    defp dictionaries() do
+      [
+        %{
+          name: "d1",
+          words: %{
+            "motherboard" => 1,
+            "mother" => 2,
+            "board" => 3,
+            "abcd" => 4,
+            "cdef" => 5
+          }
+        },
+        %{
+          name: "d2",
+          words: %{
+            "z" => 1,
+            "8" => 2,
+            "99" => 3,
+            "$" => 4,
+            "asdf1234&*" => 5
+          }
+        }
+      ]
+    end
+
+    defp dict_match() do
+      %{
+        pattern: "dictionary",
+        i: 0,
+        j: 0,
+        token: "",
+        matched_word: "",
+        rank: 0,
+        dictionary_name: "",
+        reversed: false,
+        l33t: false
+      }
+    end
+  end
 end
