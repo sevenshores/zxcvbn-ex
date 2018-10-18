5528791506672bde647cdfeee4f771c06121ab1c
Kris Lamote <kris.lamote@it-kitchen.be>
2018-10-18 22:23:12 +0200

    dictionary parsing

--- test/zxcvbn/data/parser_test.exs
+++ test/zxcvbn/data/parser_test.exs
@@ -1,3 +1,17 @@
 defmodule Zxcvbn.Data.ParserTest do
   use ExUnit.Case
+
+  import Zxcvbn.Data.Parser, only: [parse_data_file: 1]
+
+  # todo as a 'setUp' create a temp file in priv/data, delete afterwards
+  describe "parse_data_file/1" do
+    # @tag :pending
+    test "" do
+      dict = parse_data_file("surnames.txt")
+      assert dict.words["smith"] == 1
+      assert dict.words["weed"] == 3908
+
+      assert dict.name == "surnames"
+    end
+  end
 end
