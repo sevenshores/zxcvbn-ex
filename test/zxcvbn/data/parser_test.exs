24b5af4520d8428981d0c0de04eb845e24221c50
Ryan Winchester <code@ryanwinchester.ca>
2018-10-26 10:49:51 -0700

    Add setup helper to parser test

--- test/zxcvbn/data/parser_test.exs
+++ test/zxcvbn/data/parser_test.exs
@@ -4,23 +4,31 @@ defmodule Zxcvbn.Data.ParserTest do
   import Zxcvbn.Data.Parser, only: [parse_data_file: 1]
 
   describe "parse_data_file/1" do
-    # @tag :pending
-    test "" do
-      # setup
-      {:ok, file} = File.open("priv/data/test.txt", [:write, :utf8])
-      IO.write(file, "first\nmiddle\nend\n")
-      File.close(file)
+    setup [:create_test_file]
 
-      dict = parse_data_file("test.txt")
+    test "parses the file", %{test_file: filename} do
+      dict = parse_data_file(filename)
       assert dict.words["first"] == 1
       assert dict.words["middle"] == 2
       assert dict.words["end"] == 3
       assert dict.words["googlygoob"] == nil
 
       assert dict.name == "test"
-
-      # teardown
-      File.rm("priv/data/test.txt")
     end
+  end
+
+  ## Setup helpers
+
+  defp create_test_file(_) do
+    "priv/data/test.txt"
+    |> File.open!([:write, :utf8])
+    |> IO.write("first\nmiddle\nend\n")
+    |> File.close()
+
+    on_exit(fn ->
+      File.rm!("priv/data/test.txt")
+    end)
+
+    {:ok, test_file: "test.txt"}
   end
 end
