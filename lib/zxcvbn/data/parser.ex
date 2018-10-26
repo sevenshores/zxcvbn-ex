42f930583e4979edc7e22795dcb12140386df8c9
KrisLamote <kris.lamote@it-kitchen.be>
2018-10-26 19:04:44 +0200

    Dict parsing (#23)
    * dictionary parsing
    
    * adding a test with on the fly dict

--- lib/zxcvbn/data/parser.ex
+++ lib/zxcvbn/data/parser.ex
@@ -2,12 +2,23 @@ defmodule Zxcvbn.Data.Parser do
   @moduledoc false
 
   def parse_data_file(filename) do
-    lists_dir()
-    |> Path.join(filename)
-    |> File.read!()
-    |> String.downcase()
-    |> String.split("\n")
-    |> List.flatten()
+    name = filename |> String.split(".") |> List.first()
+
+    {words, _} =
+      lists_dir()
+      |> Path.join(filename)
+      |> File.read!()
+      |> String.downcase()
+      |> String.split("\n")
+      |> List.flatten()
+      |> Enum.reduce({%{}, 1}, &add_and_count(&1, &2))
+
+    %{name: name, words: words}
+  end
+
+  defp add_and_count(word, {words, cnt}) do
+    words = Map.update(words, word, cnt, & &1)
+    {words, cnt + 1}
   end
 
   defp lists_dir, do: Application.app_dir(:zxcvbn, ~w(priv data))
--- test/zxcvbn/data/parser_test.exs
+++ test/zxcvbn/data/parser_test.exs
@@ -1,3 +1,26 @@
 defmodule Zxcvbn.Data.ParserTest do
   use ExUnit.Case
+
+  import Zxcvbn.Data.Parser, only: [parse_data_file: 1]
+
+  describe "parse_data_file/1" do
+    # @tag :pending
+    test "" do
+      # setup
+      {:ok, file} = File.open("priv/data/test.txt", [:write, :utf8])
+      IO.write(file, "first\nmiddle\nend\n")
+      File.close(file)
+
+      dict = parse_data_file("test.txt")
+      assert dict.words["first"] == 1
+      assert dict.words["middle"] == 2
+      assert dict.words["end"] == 3
+      assert dict.words["googlygoob"] == nil
+
+      assert dict.name == "test"
+
+      # teardown
+      File.rm("priv/data/test.txt")
+    end
+  end
 end
